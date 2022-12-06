0459de34d7927f6b442ec04dd7830546c3894b1e
Zylar Mccullah <zylarmccullah@foresthills.local>
2022-12-06 12:24:47 -0500

    Commenting better

--- JStoPS.js
+++ JStoPS.js
@@ -1,6 +1,6 @@
 // Create an ActiveX object for the PowerShell engine
 var shell = new ActiveXObject("PowerShell.Application");
 
-// Run the PowerShell script
+// Run the PowerShell script ---- Set powershell script to grab and execute.
 var command = "& 'C:\Users\zylarmccullah\Documents\Code\Automation\Win10AutoUpdate.ps1'";
 shell.Execute(command);
